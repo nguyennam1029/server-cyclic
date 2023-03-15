5c976c98fcb0d09d16ff87c9cab6673656e989ed
-_- <79126049+nguyennam1029@users.noreply.github.com>
2023-03-15 20:25:40 +0700

    Create index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
 
-server.use(middlewares);
-server.use(router);
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
 
-server.listen(port);
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
